32cdac40e2b090e3dc944e569bc7dcdc8c492c51
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-04-04 22:40:56 -0700

    Update tipCalculator_v.0.1.1.py

--- tipCalculator_v.0.1.1.py
+++ tipCalculator_v.0.1.1.py
@@ -1,17 +1,21 @@
 
-#tip calculator
+class tipCalculator:
 
-#Where you enter you tab 
-x = float(input("Enter Your Bill Amount: "))
-#Where you can enter you tip in fomrat 10% or 10
-y = str(input("Enter Tip Percent: "))
-
-if y[-1] == '%':
-    y = y[:-1]
-    print(y)#prints y again but with the % symbol missing this time around 
-    y = "." + y #how we add the decimal point to the front of our interger string
-    print(y) #shows you how y looks after with the decimal in front 
-    print("{:0.2f}".format(x*float(y))) # your tip calculated based on the bill and tip percentage
-elif y:
-    y = "." + y
-    print("{:0.2f}".format(x*float(y)))
+    def __init__(self,invoiceAmount,tipPercent):
+        self.invoiceAmount = invoiceAmount
+        self.tipPercent = tipPercent
+        
+    def tipFormat(self):
+        if self.tipPercent[-1] == '%':
+            self.tipPercent = self.tipPercent[:-1]
+            x = float(self.invoiceAmount)*(float(self.tipPercent)/100)
+            if x.is_integer():
+                return int(x)
+            elif x != x.is_integer():
+                return round(x,2)
+   
+    
+userInput1 = str(input("Please Enter Your Amount :"))
+userInput2 = str(input("Please Enter Tip Percentage")) 
+tp = tipCalculator(userInput1,userInput2)
+print(tp.tipFormat())
