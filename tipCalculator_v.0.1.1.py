64d8b547e145d06287bc0438a39b1e92fa086c52
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2020-04-04 22:57:45 -0700

    Update tipCalculator_v.0.1.1.py

--- tipCalculator_v.0.1.1.py
+++ tipCalculator_v.0.1.1.py
@@ -10,11 +10,19 @@ class tipCalculator:
             x = float(self.invoiceAmount)*(float(self.tipPercent)/100)
             if x.is_integer():
                 x = int(x)
-                print("Your Tip Due is: " + "$"+str(x)+".00")
+                print("Your tip amount due is: " + "$"+str(x)+".00")
             elif x != x.is_integer():
-                print(round(x,2))
+                print("Your tip amount due is: " + str(round(x,2)))
+
+        elif self.tipPercent[-1] != '%':
+            x = float(self.invoiceAmount)*(float(self.tipPercent)/100)
+            if x.is_integer():
+                x = int(x)
+                print("Your tip amount due is: " + "$"+str(x)+".00")
+            elif x != x.is_integer():
+                print("Your tip amount due is: " + str(round(x,2)))
    
-    
+#----------------------------------------------Main-----------------------------------------------------------  
 userInput1 = str(input("Please Enter Your Amount: "))
 userInput2 = str(input("Please Enter Tip Percentage: ")) 
 tp = tipCalculator(userInput1,userInput2)
